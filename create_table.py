f9b6d7d889fe76580fe2a8df27403a0c22b34886
king-slayer-00 <97957777+king-slayer-00@users.noreply.github.com>
2022-01-23 15:42:10 +0530

    Updated

--- create_table.py
+++ create_table.py
@@ -6,10 +6,7 @@ cursor = connection.cursor()
 
 try:
     with connection.cursor() as cursor:
-        cursor.execute('create table students\
-                       \(name varchar(150) not null,\
-                       \id int(20) not null primary key,\
-                           \department_name varchar(150) not null)')
+        cursor.execute('create table students(name varchar(150) not null, id int(20) not null primary key, department_name varchar(150) not null)')
  
 finally:
     connection.close()
